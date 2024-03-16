b73b1aec65cc174600c0a93625d6bc60dbbc9e75
phattran.2002.work@gmail.com <>
2024-03-16 09:49:12 +0700

    Init backend

---
+++ main.py
@@ -0,0 +1,26 @@
+
+from gramformer import Gramformer
+import torch
+from fastapi import FastAPI
+import streamlit as st
+
+
+def set_seed(seed):
+  torch.manual_seed(seed)
+  if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(seed)
+
+set_seed(1212)
+
+
+gf = Gramformer(models = 1, use_gpu=False) # 1=corrector, 2=detector
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/correct-sentence")
+def correctSentence(q: str = None):
+   corrected_sentences = gf.correct(q, max_candidates=1)
+   return list(corrected_sentences)[0]
